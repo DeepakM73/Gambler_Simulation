d9893921e32b22aeb3a976b06b0377df8ad8fc78
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-15 08:31:31 +0530

    Added: Stake of the Gambler

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -2,7 +2,10 @@ package com.bridgelabz.gambler;
 
 public class Gambler {
 
+    public static int INITIALSTAKE = 100;
+    public static int BET_PER_GAME = 1;
+
     public static void main(String[] args) {
-	System.out.print("Welcome to Gambler Simulation");
+	
     }
 }
