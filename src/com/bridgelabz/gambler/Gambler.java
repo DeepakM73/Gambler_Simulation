195c8dfabdc97adec0cdfbb38941231320454eac
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-15 09:00:01 +0530

    Added: Gambler bets to win or loss

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -2,10 +2,23 @@ package com.bridgelabz.gambler;
 
 public class Gambler {
 
-    public static int INITIALSTAKE = 100;
-    public static int BET_PER_GAME = 1;
+    public static final int INITIALSTAKE = 100;
+    public static final int BET_PER_GAME = 1;
+
+    public static void stakeWinOrLoss() {
+        if(Math.random() > 0.5){
+            int win = INITIALSTAKE + 1;
+            System.out.println("Won the bet :"+win);
+        }
+        else{
+            int loss = INITIALSTAKE -1;
+            System.out.println("Lost the bet :"+loss);
+        }
+    }
 
     public static void main(String[] args) {
-	
+        System.out.println("The Gambler Starts Betting");
+        stakeWinOrLoss();
     }
+
 }
