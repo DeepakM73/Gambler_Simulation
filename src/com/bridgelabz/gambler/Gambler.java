1ca105ca0b2d8e73c842a91bbd829a5b03e883ce
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-20 07:34:38 +0530

    Added: 20 days of Betting

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -3,7 +3,7 @@ package com.bridgelabz.gambler;
 public class Gambler {
 
     public static final int INITIAL_STAKE = 100;
-    public static final int BETSTAKE = 1;
+    public static final int BET_STAKE = 1;
     public static int finalSakeForDay, initialStakeForDay;
 
 
@@ -23,11 +23,6 @@ public class Gambler {
         return initialStakeForDay;
     }
 
-    /**
-     * Betting for single day only
-     *
-     * @return finalStakeForDay
-     */
     public static int resignationForDay() {
 
         int lowerAmount = INITIAL_STAKE - INITIAL_STAKE / 2;
@@ -47,10 +42,25 @@ public class Gambler {
         return finalSakeForDay;
     }
 
+    public static int stakeAtDay21() {
+
+        int finalSakeAfterDay20 = 0;
+        int profitOrloss;
+
+        for (int i = 1; i <= 20; i++) {
+
+            System.out.println("Day: " + i);
+            resignationForDay();
+            finalSakeAfterDay20 += finalSakeForDay;
+        }
+        profitOrloss = finalSakeAfterDay20 - INITIAL_STAKE * 20;
+        return profitOrloss;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Problem");
 
-        System.out.println("Gambler reached to " + resignationForDay() + " and stopped for day");
+        System.out.println("Profit or loss after day 20: " + stakeAtDay21());
     }
 }
 
