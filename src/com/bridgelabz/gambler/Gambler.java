cd94e8660cdee14a9abc3b4336a70db425c213cf
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-20 07:31:35 +0530

    Added: Calculative Gambler Conditions

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -2,23 +2,56 @@ package com.bridgelabz.gambler;
 
 public class Gambler {
 
-    public static final int INITIALSTAKE = 100;
-    public static final int BET_PER_GAME = 1;
+    public static final int INITIAL_STAKE = 100;
+    public static final int BETSTAKE = 1;
+    public static int finalSakeForDay, initialStakeForDay;
 
-    public static void stakeWinOrLoss() {
-        if(Math.random() > 0.5){
-            int win = INITIALSTAKE + 1;
-            System.out.println("Won the bet :"+win);
+
+
+    public static int makeBet() {
+
+        int WIN = 1;
+        int random = (int) (Math.random() * 2);
+
+        if (random == WIN) {
+            System.out.println("***WON***");
+            initialStakeForDay++;
+        } else {
+            System.out.println("***LOSE***");
+            initialStakeForDay--;
         }
-        else{
-            int loss = INITIALSTAKE -1;
-            System.out.println("Lost the bet :"+loss);
+        return initialStakeForDay;
+    }
+
+    /**
+     * Betting for single day only
+     *
+     * @return finalStakeForDay
+     */
+    public static int resignationForDay() {
+
+        int lowerAmount = INITIAL_STAKE - INITIAL_STAKE / 2;
+        int upperAmount = INITIAL_STAKE + INITIAL_STAKE / 2;
+        initialStakeForDay = 100;
+        boolean run = true;
+
+        while (run) {
+
+            makeBet();
+            if (initialStakeForDay <= lowerAmount)
+                run = false;
+            if (initialStakeForDay >= upperAmount)
+                run = false;
         }
+        finalSakeForDay = initialStakeForDay;
+        return finalSakeForDay;
     }
 
     public static void main(String[] args) {
-        System.out.println("The Gambler Starts Betting");
-        stakeWinOrLoss();
-    }
+        System.out.println("Welcome to Gambling Problem");
 
+        System.out.println("Gambler reached to " + resignationForDay() + " and stopped for day");
+    }
 }
+
+
